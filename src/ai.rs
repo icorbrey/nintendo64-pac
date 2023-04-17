6bfb34f3dd5d39748b9d48802d24b67e20e4c24e
Isaac <icorbrey@gmail.com>
2023-04-17 17:59:07 -0400

    fix: Wrong access types for some registers

--- src/ai.rs
+++ src/ai.rs
@@ -1,13 +1,16 @@
-use tock_registers::{register_structs, registers::ReadWrite};
+use tock_registers::{
+    register_structs,
+    registers::{ReadWrite, WriteOnly},
+};
 
 register_structs! {
     pub AudioInterfaceRegisters {
-        (0x0000 => pub dram_address: ReadWrite<u32>),
+        (0x0000 => pub dram_address: WriteOnly<u32>),
         (0x0004 => pub length: ReadWrite<u32>),
-        (0x0008 => pub control: ReadWrite<u32>),
+        (0x0008 => pub control: WriteOnly<u32>),
         (0x000C => pub status: ReadWrite<u32>),
-        (0x0010 => pub dacrate: ReadWrite<u32>),
-        (0x0014 => pub bitrate: ReadWrite<u32>),
+        (0x0010 => pub dacrate: WriteOnly<u32>),
+        (0x0014 => pub bitrate: WriteOnly<u32>),
         (0x0018 => @END),
     }
 }
--- src/mi.rs
+++ src/mi.rs
@@ -1,10 +1,13 @@
-use tock_registers::{register_structs, registers::ReadWrite};
+use tock_registers::{
+    register_structs,
+    registers::{ReadOnly, ReadWrite},
+};
 
 register_structs! {
     pub MipsInterfaceRegisters {
         (0x0000 => pub init_mode: ReadWrite<u32>),
-        (0x0004 => pub version: ReadWrite<u32>),
-        (0x0008 => pub interrupt: ReadWrite<u32>),
+        (0x0004 => pub version: ReadOnly<u32>),
+        (0x0008 => pub interrupt: ReadOnly<u32>),
         (0x000C => pub interrupt_mask: ReadWrite<u32>),
         (0x0010 => @END),
     }
