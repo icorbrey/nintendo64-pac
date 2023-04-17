42c47500db3f1706a6366fb0217d685ba621538e
Isaac <icorbrey@gmail.com>
2023-04-17 19:57:10 -0400

    feat: Add DPS bitfields

--- src/dps.rs
+++ src/dps.rs
@@ -1,11 +1,35 @@
-use tock_registers::{register_structs, registers::ReadWrite};
+use tock_registers::{register_bitfields, register_structs, registers::ReadWrite};
 
 register_structs! {
-    pub DpcRegisters {
-        (0x0000 => pub tmem_bist: ReadWrite<u32>),
-        (0x0004 => pub test_mode: ReadWrite<u32>),
-        (0x0008 => pub buffer_test_address: ReadWrite<u32>),
-        (0x000C => pub buffer_test_data: ReadWrite<u32>),
+    pub DpsRegisters {
+        (0x0000 => pub tmem_bist: ReadWrite<u32, TmemBist::Register>),
+        (0x0004 => pub buffer_test_mode: ReadWrite<u32, BufferTestMode::Register>),
+        (0x0008 => pub buffer_test_address: ReadWrite<u32, BufferTestAddress::Register>),
+        (0x000C => pub buffer_test_data: ReadWrite<u32, BufferTestData::Register>),
         (0x0010 => @END),
     }
+}
+
+register_bitfields! {
+    u32,
+
+    TmemBist [
+        BIST_CHECK OFFSET(0) NUMBITS(1)  [],
+        BIST_GO    OFFSET(1) NUMBITS(1)  [],
+        BIST_CLEAR OFFSET(2) NUMBITS(1)  [],
+        BIST_DONE  OFFSET(2) NUMBITS(1)  [],
+        BIST_FAIL  OFFSET(3) NUMBITS(8)  []
+    ],
+
+    BufferTestMode [
+        TEST_MODE  OFFSET(0) NUMBITS(1)  []
+    ],
+
+    BufferTestAddress [
+        TEST_ADDR  OFFSET(0) NUMBITS(7)  []
+    ],
+
+    BufferTestData [
+        TEST_DATA  OFFSET(0) NUMBITS(32) []
+    ]
 }
