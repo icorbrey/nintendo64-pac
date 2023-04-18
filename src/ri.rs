8bb112bdcb7459d93154aeaaa176b3d415473990
Isaac <icorbrey@gmail.com>
2023-04-17 21:48:58 -0400

    feat: Add RDRAM interface bitfields

--- src/ri.rs
+++ src/ri.rs
@@ -1,18 +1,55 @@
 use tock_registers::{
-    register_structs,
+    register_bitfields, register_structs,
     registers::{ReadOnly, ReadWrite, WriteOnly},
 };
 
 register_structs! {
     pub RdramRegisters {
-        (0x0000 => pub mode: ReadWrite<u32>),
-        (0x0004 => pub config: ReadWrite<u32>),
+        (0x0000 => pub mode: ReadWrite<u32, Mode::Register>),
+        (0x0004 => pub config: ReadWrite<u32, Config::Register>),
         (0x0008 => pub current_load: WriteOnly<u32>),
-        (0x000C => pub select: ReadWrite<u32>),
-        (0x0010 => pub refresh: ReadWrite<u32>),
-        (0x0014 => pub latency: ReadWrite<u32>),
-        (0x0018 => pub read_error: ReadOnly<u32>),
+        (0x000C => pub select: ReadWrite<u32, Select::Register>),
+        (0x0010 => pub refresh: ReadWrite<u32, Refresh::Register>),
+        (0x0014 => pub latency: ReadWrite<u32, Latency::Register>),
+        (0x0018 => pub read_error: ReadOnly<u32, ReadError::Register>),
         (0x001C => pub write_error: WriteOnly<u32>),
         (0x0020 => @END),
     }
+}
+
+register_bitfields! {
+    u32,
+
+    Mode [
+        OPERATING_MODE         OFFSET(0)  NUMBITS(2) [],
+        STOP_TRANSMIT_ACTIVE   OFFSET(2)  NUMBITS(1) [],
+        STOP_RECEIVE_ACTIVE    OFFSET(3)  NUMBITS(1) [],
+    ],
+
+    Config [
+        CURRENT_CONTROL_INPUT  OFFSET(0)  NUMBITS(6) [],
+        CURRENT_CONTROL_ENABLE OFFSET(6)  NUMBITS(1) [],
+    ],
+
+    Select [
+        RECEIVE_SELECT         OFFSET(0)  NUMBITS(2) [],
+        TRANSMIT_SELECT        OFFSET(2)  NUMBITS(2) [],
+    ],
+
+    Refresh [
+        CLEAN_REFRESH_DELAY    OFFSET(0)  NUMBITS(8) [],
+        DIRTY_REFRESH_DELAY    OFFSET(8)  NUMBITS(8) [],
+        REFRESH_BANK           OFFSET(16) NUMBITS(1) [],
+        REFRESH_ENABLE         OFFSET(17) NUMBITS(1) [],
+        REFRESH_OPTIMIZE       OFFSET(18) NUMBITS(1) [],
+    ],
+
+    Latency [
+        DMA_LATENCY            OFFSET(0)  NUMBITS(4) [],
+    ],
+
+    ReadError [
+        NACK_ERROR             OFFSET(0)  NUMBITS(1) [],
+        ACK_ERROR              OFFSET(1)  NUMBITS(1) [],
+    ]
 }
