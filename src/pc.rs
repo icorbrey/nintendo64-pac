fe1c5320fd7ede684b4663e4dd15facfdd9000c2
Isaac <icorbrey@gmail.com>
2023-04-20 00:40:37 -0400

    feat: Make program counter system not access static memory in development

--- src/pc.rs
+++ src/pc.rs
@@ -1,15 +1,49 @@
 use tock_registers::{register_bitfields, register_structs, registers::ReadWrite};
 
+use crate::HARDWARE;
+
+/// The static address of the Nintendo 64's RDRAM interface registers.
+#[cfg(target_vendor = "nintendo64")]
 const PC_REGS_BASE: usize = 0x0408_0000;
+
+#[cfg(not(target_vendor = "nintendo64"))]
+lazy_static::lazy_static! {
+    /// A registry access analogue for development and testing.
+    ///
+    /// We have to modify the registry access mechanism when building for
+    /// architectures other than the Nintendo 64 since the production registry
+    /// access mechanism accesses a static memory location. This is disallowed
+    /// on modern operating systems, so we instead dynamically allocate the
+    /// memory so that testing and development can occur.
+    static ref REGISTERS: ProgramCounterRegisters = unsafe { std::mem::zeroed() };
+}
 
 #[non_exhaustive]
 pub struct ProgramCounter;
 
 impl ProgramCounter {
+    /// Gets a reference to the program counter registers.
+    #[cfg(target_vendor = "nintendo64")]
     fn registers<'a>(&self) -> &'a ProgramCounterRegisters {
         unsafe { &mut *(PC_REGS_BASE as *mut ProgramCounterRegisters) }
     }
+
+    /// Gets a reference to the program counter registers.
+    #[cfg(not(target_vendor = "nintendo64"))]
+    fn registers<'a>(&self) -> &'a REGISTERS {
+        &REGISTERS
+    }
+
+    /// Returns ownership of the program counter registers to
+    /// [`HARDWARE`][crate::HARDWARE].
+    pub fn drop(self) {
+        unsafe { HARDWARE.program_counter.drop(self) }
+    }
 }
+
+// This is a hack to allow code to run for development.
+#[cfg(not(target_vendor = "nintendo64"))]
+unsafe impl Sync for ProgramCounterRegisters {}
 
 register_structs! {
     ProgramCounterRegisters {
