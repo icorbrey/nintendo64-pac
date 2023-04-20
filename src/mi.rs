6b77f9f1b03fb1936fbddb4cd0199b60808324c4
Isaac <icorbrey@gmail.com>
2023-04-19 21:55:30 -0400

    feat: Make MIPS interface not access static memory in development

--- src/mi.rs
+++ src/mi.rs
@@ -3,18 +3,44 @@ use tock_registers::{
     registers::{ReadOnly, ReadWrite},
 };
 
+/// The static address of the Nintendo 64's MIPS interface registers.
+#[cfg(target_vendor = "nintendo64")]
 const MI_REGS_BASE: usize = 0x0430_0000;
+
+#[cfg(not(target_vendor = "nintendo64"))]
+lazy_static::lazy_static! {
+    /// A registry access analogue for development and testing.
+    ///
+    /// We have to modify the registry access mechanism when building for
+    /// architectures other than the Nintendo 64 since the production registry
+    /// access mechanism accesses a static memory location. This is disallowed
+    /// on modern operating systems, so we instead dynamically allocate the
+    /// memory so that testing and development can occur.
+    static ref REGISTERS: MipsInterfaceRegisters = unsafe { std::mem::zeroed() };
+}
 
 pub struct MipsInterface;
 
 impl MipsInterface {
-    pub fn registers<'a>(&self) -> &'a MipsInterfaceRegisters {
+    /// Gets a reference to the MIPS interface registers.
+    #[cfg(target_vendor = "nintendo64")]
+    fn registers<'a>(&self) -> &'a MipsInterfaceRegisters {
         unsafe { &mut *(MI_REGS_BASE as *mut MipsInterfaceRegisters) }
+    }
+
+    /// Returns a reference to the MIPS interface registers.
+    #[cfg(not(target_vendor = "nintendo64"))]
+    fn registers<'a>(&self) -> &'a REGISTERS {
+        &REGISTERS
     }
 }
 
+// This is a hack to allow code to run for development.
+#[cfg(not(target_vendor = "nintendo64"))]
+unsafe impl Sync for MipsInterfaceRegisters {}
+
 register_structs! {
-    pub MipsInterfaceRegisters {
+    MipsInterfaceRegisters {
         (0x0000 => pub init_mode: ReadWrite<u32, InitMode::Register>),
         (0x0004 => pub version: ReadOnly<u32, Version::Register>),
         (0x0008 => pub interrupts: ReadOnly<u32, Interrupts::Register>),
@@ -26,7 +52,7 @@ register_structs! {
 register_bitfields! {
     u32,
 
-    pub InitMode [
+    InitMode [
         INIT_LENGTH             OFFSET(0)  NUMBITS(7) [],
         INIT_MODE               OFFSET(7)  NUMBITS(1) [],
         EBUS_TEST_MODE          OFFSET(8)  NUMBITS(1) [],
@@ -41,14 +67,14 @@ register_bitfields! {
         SET_RDRAM_REG           OFFSET(13) NUMBITS(1) [],
     ],
 
-    pub Version [
+    Version [
         IO                      OFFSET(0)  NUMBITS(8) [],
         RAC                     OFFSET(8)  NUMBITS(8) [],
         RDP                     OFFSET(16) NUMBITS(8) [],
         RSP                     OFFSET(24) NUMBITS(8) [],
     ],
 
-    pub Interrupts [
+    Interrupts [
         SP_INTERRUPT            OFFSET(0)  NUMBITS(1) [],
         SI_INTERRUPT            OFFSET(1)  NUMBITS(1) [],
         AI_INTERRUPT            OFFSET(2)  NUMBITS(1) [],
@@ -57,7 +83,7 @@ register_bitfields! {
         DP_INTERRUPT            OFFSET(5)  NUMBITS(1) [],
     ],
 
-    pub InterruptMasks [
+    InterruptMasks [
         SP_INTERRUPT_MASK       OFFSET(0)  NUMBITS(1) [],
         SI_INTERRUPT_MASK       OFFSET(1)  NUMBITS(1) [],
         AI_INTERRUPT_MASK       OFFSET(2)  NUMBITS(1) [],
