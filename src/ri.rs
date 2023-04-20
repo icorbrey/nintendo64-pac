ba75124b6febeee962d86d32668a1da07eb92ee0
Isaac <icorbrey@gmail.com>
2023-04-20 00:27:53 -0400

    feat: Make RDRAM interface not access static memory in development

--- src/ri.rs
+++ src/ri.rs
@@ -3,18 +3,52 @@ use tock_registers::{
     registers::{ReadOnly, ReadWrite, WriteOnly},
 };
 
+use crate::HARDWARE;
+
+/// The static address of the Nintendo 64's RDRAM interface registers.
+#[cfg(target_vendor = "nintendo64")]
 const RI_REGS_BASE: usize = 0x0470_0000;
+
+#[cfg(not(target_vendor = "nintendo64"))]
+lazy_static::lazy_static! {
+    /// A registry access analogue for development and testing.
+    ///
+    /// We have to modify the registry access mechanism when building for
+    /// architectures other than the Nintendo 64 since the production registry
+    /// access mechanism accesses a static memory location. This is disallowed
+    /// on modern operating systems, so we instead dynamically allocate the
+    /// memory so that testing and development can occur.
+    static ref REGISTERS: RdramInterfaceRegisters = unsafe { std::mem::zeroed() };
+}
 
 pub struct RdramInterface;
 
 impl RdramInterface {
+    /// Gets a reference to the RDRAM interface registers.
+    #[cfg(target_vendor = "nintendo64")]
     pub fn registers<'a>(&self) -> &'a RdramInterfaceRegisters {
         unsafe { &mut *(RI_REGS_BASE as *mut RdramInterfaceRegisters) }
     }
+
+    /// Gets a reference to the RDRAM interface registers.
+    #[cfg(not(target_vendor = "nintendo64"))]
+    fn registers<'a>(&self) -> &'a REGISTERS {
+        &REGISTERS
+    }
+
+    /// Returns ownership of the RDRAM interface registers to
+    /// [`HARDWARE`][crate::HARDWARE].
+    pub fn drop(self) {
+        unsafe { HARDWARE.rdram_interface.drop(self) }
+    }
 }
 
+// This is a hack to allow code to run for development.
+#[cfg(not(target_vendor = "nintendo64"))]
+unsafe impl Sync for RdramInterfaceRegisters {}
+
 register_structs! {
-    pub RdramInterfaceRegisters {
+    RdramInterfaceRegisters {
         (0x0000 => pub mode: ReadWrite<u32, Mode::Register>),
         (0x0004 => pub config: ReadWrite<u32, Config::Register>),
         (0x0008 => pub current_load: WriteOnly<u32>),
@@ -30,23 +64,23 @@ register_structs! {
 register_bitfields! {
     u32,
 
-    pub Mode [
+    Mode [
         OPERATING_MODE         OFFSET(0)  NUMBITS(2) [],
         STOP_TRANSMIT_ACTIVE   OFFSET(2)  NUMBITS(1) [],
         STOP_RECEIVE_ACTIVE    OFFSET(3)  NUMBITS(1) [],
     ],
 
-    pub Config [
+    Config [
         CURRENT_CONTROL_INPUT  OFFSET(0)  NUMBITS(6) [],
         CURRENT_CONTROL_ENABLE OFFSET(6)  NUMBITS(1) [],
     ],
 
-    pub Select [
+    Select [
         RECEIVE_SELECT         OFFSET(0)  NUMBITS(2) [],
         TRANSMIT_SELECT        OFFSET(2)  NUMBITS(2) [],
     ],
 
-    pub Refresh [
+    Refresh [
         CLEAN_REFRESH_DELAY    OFFSET(0)  NUMBITS(8) [],
         DIRTY_REFRESH_DELAY    OFFSET(8)  NUMBITS(8) [],
         REFRESH_BANK           OFFSET(16) NUMBITS(1) [],
@@ -54,11 +88,11 @@ register_bitfields! {
         REFRESH_OPTIMIZE       OFFSET(18) NUMBITS(1) [],
     ],
 
-    pub Latency [
+    Latency [
         DMA_LATENCY            OFFSET(0)  NUMBITS(4) [],
     ],
 
-    pub ReadError [
+    ReadError [
         NACK_ERROR             OFFSET(0)  NUMBITS(1) [],
         ACK_ERROR              OFFSET(1)  NUMBITS(1) [],
     ]
