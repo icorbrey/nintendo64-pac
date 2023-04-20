e215cdca37b2608fda5cc4adf915a17c48dc03a8
Isaac <icorbrey@gmail.com>
2023-04-20 00:25:45 -0400

    feat: Make RDRAM system not access static memory in development

--- src/rdram.rs
+++ src/rdram.rs
@@ -1,17 +1,50 @@
 use tock_registers::{register_structs, registers::ReadWrite};
 
+use crate::HARDWARE;
+
+/// The static address of the Nintendo 64's RDRAM registers.
+#[cfg(target_vendor = "nintendo64")]
 const RDRAM_REGS_BASE: usize = 0x03F0_0000;
+
+#[cfg(not(target_vendor = "nintendo64"))]
+lazy_static::lazy_static! {
+    /// A registry access analogue for development and testing.
+    ///
+    /// We have to modify the registry access mechanism when building for
+    /// architectures other than the Nintendo 64 since the production registry
+    /// access mechanism accesses a static memory location. This is disallowed
+    /// on modern operating systems, so we instead dynamically allocate the
+    /// memory so that testing and development can occur.
+    static ref REGISTERS: RdramRegisters = unsafe { std::mem::zeroed() };
+}
 
 pub struct Rdram;
 
 impl Rdram {
+    /// Gets a reference to the RDRAM registers.
+    #[cfg(target_vendor = "nintendo64")]
     pub fn registers<'a>(&self) -> &'a RdramRegisters {
         unsafe { &mut *(RDRAM_REGS_BASE as *mut RdramRegisters) }
     }
+
+    /// Returns a reference to the RDRAM registers.
+    #[cfg(not(target_vendor = "nintendo64"))]
+    fn registers<'a>(&self) -> &'a REGISTERS {
+        &REGISTERS
+    }
+
+    /// Returns ownership of the RDRAM registers to [`HARDWARE`][crate::HARDWARE].
+    pub fn drop(self) {
+        unsafe { HARDWARE.rdram.drop(self) }
+    }
 }
 
+// This is a hack to allow code to run for development.
+#[cfg(not(target_vendor = "nintendo64"))]
+unsafe impl Sync for RdramRegisters {}
+
 register_structs! {
-    pub RdramRegisters {
+    RdramRegisters {
         (0x0000 => pub config: ReadWrite<u32>),
         (0x0004 => pub device_id: ReadWrite<u32>),
         (0x0008 => pub delay: ReadWrite<u32>),
