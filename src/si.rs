32dd657e1617a5aa6258fbf3cfac2508c4a734f4
Isaac <icorbrey@gmail.com>
2023-04-20 00:29:41 -0400

    feat: Make serial interface not access static memory in development

--- src/si.rs
+++ src/si.rs
@@ -3,18 +3,53 @@ use tock_registers::{
     registers::{ReadWrite, WriteOnly},
 };
 
+use crate::HARDWARE;
+
+/// The static address of the Nintendo 64's serial interface registers.
+#[cfg(target_vendor = "nintendo64")]
 const SI_REGS_BASE: usize = 0x0460_0000;
 
+#[cfg(not(target_vendor = "nintendo64"))]
+lazy_static::lazy_static! {
+    /// A registry access analogue for development and testing.
+    ///
+    /// We have to modify the registry access mechanism when building for
+    /// architectures other than the Nintendo 64 since the production registry
+    /// access mechanism accesses a static memory location. This is disallowed
+    /// on modern operating systems, so we instead dynamically allocate the
+    /// memory so that testing and development can occur.
+    static ref REGISTERS: SerialInterfaceRegisters = unsafe { std::mem::zeroed() };
+}
+
+#[non_exhaustive]
 pub struct SerialInterface;
 
 impl SerialInterface {
+    /// Gets a reference to the serial interface registers.
+    #[cfg(target_vendor = "nintendo64")]
     pub fn registers<'a>(&self) -> &'a SerialInterfaceRegisters {
         unsafe { &mut *(SI_REGS_BASE as *mut SerialInterfaceRegisters) }
     }
+
+    /// Returns a reference to the serial interface registers.
+    #[cfg(not(target_vendor = "nintendo64"))]
+    fn registers<'a>(&self) -> &'a REGISTERS {
+        &REGISTERS
+    }
+
+    /// Returns ownership of the serial interface registers to
+    /// [`HARDWARE`][crate::HARDWARE].
+    pub fn drop(self) {
+        unsafe { HARDWARE.serial_interface.drop(self) }
+    }
 }
 
+// This is a hack to allow code to run for development.
+#[cfg(not(target_vendor = "nintendo64"))]
+unsafe impl Sync for SerialInterfaceRegisters {}
+
 register_structs! {
-    pub SerialInterfaceRegisters {
+    SerialInterfaceRegisters {
         (0x0000 => pub dram_address: ReadWrite<u32, DmaAddress::Register>),
         (0x0004 => pub read_64_bits: WriteOnly<u32>),
         (0x0008 => _reserved0),
@@ -28,11 +63,11 @@ register_structs! {
 register_bitfields! {
     u32,
 
-    pub DmaAddress [
+    DmaAddress [
         ADDRESS      OFFSET(0)  NUMBITS(24) [],
     ],
 
-    pub Status [
+    Status [
         DMA_BUSY     OFFSET(0)  NUMBITS(1)  [],
         IO_READ_BUSY OFFSET(1)  NUMBITS(1)  [],
         DMA_ERROR    OFFSET(3)  NUMBITS(1)  [],
