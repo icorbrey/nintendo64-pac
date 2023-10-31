75695b568bae0c9ce49a90fa6776e38f7161f82d
Isaac <icorbrey@gmail.com>
2023-10-31 18:22:55 -0400

    wip

--- src/mi.rs
+++ src/mi.rs
@@ -4,50 +4,35 @@ use core::ops::Deref;
 
 use proc_bitfield::bitfield;
 
-const MI_BASE_REG: u32 = 0x0430_0000;
+use crate::{impl_deref, impl_get, impl_interface, impl_set};
 
-/// MIPS interface.
+/// # MIPS Interface
 pub struct Mi;
 
-impl Mi {
-    pub fn ptr() -> *const MiRegisters {
-        MI_BASE_REG as *const _
-    }
-}
+impl_interface!(Mi, MiRegisters, 0x0430_0000);
 
-unsafe impl Sync for Mi {}
-
-impl Deref for Mi {
-    type Target = MiRegisters;
-
-    fn deref(&self) -> &Self::Target {
-        unsafe { &*Self::ptr() }
-    }
-}
-
-/// MIPS interface register block.
+/// # MIPS Interface Register Block
 #[repr(C)]
 pub struct MiRegisters {
-    /// 0x00 - Init mode
+    /// `0x00` - Init Mode
     pub mi_init_mode_reg: MiInitModeReg,
 
-    /// 0x04 - Version
+    /// `0x04` - Version
     pub mi_version_reg: MiVersionReg,
 
-    /// 0x08 - Interrupts
+    /// `0x08` - Interrupts
     pub mi_intr_reg: MiIntrReg,
 
-    /// 0x0C - Interrupt masks
+    /// `0x0C` - Interrupt Masks
     pub mi_intr_mask_reg: MiIntrMaskReg,
 }
 
 bitfield! {
-    /// MIPS interface init mode register.
+    /// # MIPS Interface Init Mode Register
     pub struct MiInitModeReg(pub u32): Debug {
-        /// Raw register access.
         pub raw: u32 @ ..,
 
-        pub init_length: u8 @ 0..7,
+        pub init_length: u8 [get InitLength, try_set InitLength] @ 0..7,
 
         pub init_mode: bool [read_only] @ 7,
         pub ebus_test_mode: bool [read_only] @ 8,
@@ -64,24 +49,20 @@ bitfield! {
 }
 
 bitfield! {
-    /// MIPS interface version register.
+    /// # MIPS Interface Version Register
     pub struct MiVersionReg(pub u32): Debug {
-        /// Raw register access.
         pub raw: u32 @ ..,
-
-        pub io: u8 [read_only] @ 0..8,
-        pub rac: u8 [read_only] @ 8..16,
-        pub rdp: u8 [read_only] @ 16..24,
-        pub rsp: u8 [read_only] @ 24..32,
+        pub io: u8 [read_only, get Version] @ 0..8,
+        pub rac: u8 [read_only, get Version] @ 8..16,
+        pub rdp: u8 [read_only, get Version] @ 16..24,
+        pub rsp: u8 [read_only, get Version] @ 24..32,
     }
 }
 
 bitfield! {
-    /// MIPS interface interrupt register.
+    /// # MIPS Interface Interrupt Register
     pub struct MiIntrReg(pub u32): Debug {
-        /// Raw register access.
         pub raw: u32 @ ..,
-
         pub sp_intr: bool [read_only] @ 0,
         pub si_intr: bool [read_only] @ 1,
         pub ai_intr: bool [read_only] @ 2,
@@ -92,18 +73,15 @@ bitfield! {
 }
 
 bitfield! {
-    /// MIPS interface interrupt mask register.
+    /// # MIPS Interface Interrupt Mask Register
     pub struct MiIntrMaskReg(pub u32): Debug {
-        /// Raw register access.
         pub raw: u32 @ ..,
-
         pub sp_intr_mask: bool [read_only] @ 0,
         pub si_intr_mask: bool [read_only] @ 1,
         pub ai_intr_mask: bool [read_only] @ 2,
         pub vi_intr_mask: bool [read_only] @ 3,
         pub pi_intr_mask: bool [read_only] @ 4,
         pub dp_intr_mask: bool [read_only] @ 5,
-
         pub clear_sp_mask: bool [write_only] @ 0,
         pub set_sp_mask: bool [write_only] @ 1,
         pub clear_si_mask: bool [write_only] @ 2,
@@ -118,3 +96,18 @@ bitfield! {
         pub set_dp_mask: bool [write_only] @ 11,
     }
 }
+
+/// # Init Length
+#[derive(Debug)]
+pub struct InitLength(pub u8);
+
+impl_deref!(InitLength, u8);
+impl_get!(InitLength, u8);
+impl_set!(InitLength, u8, 0..7);
+
+/// # Version
+#[derive(Debug)]
+pub struct Version(pub u8);
+
+impl_deref!(Version, u8);
+impl_get!(Version, u8);
