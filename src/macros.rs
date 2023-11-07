3984a9d84a4329af4d63d638d6411aef22926d3f
Isaac <icorbrey@gmail.com>
2023-11-07 10:13:26 -0500

    wip

--- src/macros.rs
+++ src/macros.rs
@@ -21,51 +21,51 @@ macro_rules! interface {
 
 #[macro_export]
 macro_rules! fields {
-    [$($(#[$($attrss:tt)*])* $t:path => $f:ident,)*] => {
+    [$($(#[$($attrss:tt)*])* $size:path => $name:ident,)*] => {
 		$(
 			$(#[$($attrss)*])*
 			#[derive(Debug)]
-			pub struct $f(pub $t);
+			pub struct $name(pub $size);
 
-			impl core::ops::Deref for $f {
-				type Target = $t;
+			impl core::ops::Deref for $name {
+				type Target = $size;
 
 				fn deref(&self) -> &Self::Target {
 					&self.0
 				}
 			}
 
-			impl From<$f> for u8 {
-				fn from(value: $f) -> Self {
+			impl From<$name> for u8 {
+				fn from(value: $name) -> Self {
 					value.into()
 				}
 			}
 
-			impl From<$f> for u16 {
-				fn from(value: $f) -> Self {
+			impl From<$name> for u16 {
+				fn from(value: $name) -> Self {
 					value.into()
 				}
 			}
 
-			impl From<$f> for u32 {
-				fn from(value: $f) -> Self {
+			impl From<$name> for u32 {
+				fn from(value: $name) -> Self {
 					value.into()
 				}
 			}
 
-			impl From<u8> for $f {
+			impl From<u8> for $name {
 				fn from(value: u8) -> Self {
 					value.into()
 				}
 			}
 
-			impl From<u16> for $f {
+			impl From<u16> for $name {
 				fn from(value: u16) -> Self {
 					value.into()
 				}
 			}
 
-			impl From<u32> for $f {
+			impl From<u32> for $name {
 				fn from(value: u32) -> Self {
 					value.into()
 				}
@@ -76,26 +76,28 @@ macro_rules! fields {
 
 #[macro_export]
 macro_rules! enums {
-    [$($(#[$($attrss:tt)*])* $t:path => $f:ident { $($v:expr => $k:ident,)* },)*] => {
+    [$($(#[$($attrss:tt)*])* $size:path => $name:ident {
+		$($value:expr => $key:ident,)*
+	},)*] => {
 		$(
 			$(#[$($attrss)*])*
 			#[derive(Debug)]
-			pub enum $f { $($k,)* }
+			pub enum $name { $($key,)* }
 
-			impl From<$t> for $f {
-				fn from(value: $t) -> Self {
+			impl From<$size> for $name {
+				fn from(value: $size) -> Self {
 					match value {
-						$($v => <$f>::$k,)*
+						$($value => <$name>::$key,)*
 						#[allow(unreachable_patterns)]
 						_ => panic!(),
 					}
 				}
 			}
 
-			impl From<$f> for $t {
-				fn from(value: $f) -> Self {
+			impl From<$name> for $size {
+				fn from(value: $name) -> Self {
 					match value {
-						$(<$f>::$k => $v,)*
+						$(<$name>::$key => $value,)*
 					}
 				}
 			}
